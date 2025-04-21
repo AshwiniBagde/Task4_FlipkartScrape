f9e56a41c13231b711c8e1c871e872789905c625
AshwiniBagde <ashwinibagde43@gmail.com>
2025-04-21 21:35:47 +0530

    Flipkart Scrape

--- FlipkartScrape.py
+++ FlipkartScrape.py
@@ -16,44 +16,44 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Find all product containers
-products = soup.find_all("div", class_="tUxRFH")
+laptops = soup.find_all("div", class_="tUxRFH")
 
 # Loop through each product and extract details
-for product in products:
+for laptop in laptops:
     # Extract laptop image URL
-    image_tag = product.select_one("img.DByuf4")
+    image_tag = laptop.select_one("img.DByuf4")
     image_url = image_tag["src"] if image_tag else "N/A"
 
-    # Extract laptop product name
-    name_tag = product.select_one("div.KzDlHZ")
+    # Extract laptop laptop name
+    name_tag = laptop.select_one("div.KzDlHZ")
     product_name = name_tag.text if name_tag else "N/A"
 
     # Extract laptop product rating
-    rating_tag = product.select_one("div.XQDdHH")
+    rating_tag = laptop.select_one("div.XQDdHH")
     product_rating = rating_tag.text if rating_tag else "N/A"
 
     # Extract laptop product review count
-    review_tag = product.select_one("span.Wphh3N")
+    review_tag = laptop.select_one("span.Wphh3N")
     review_count = review_tag.text.strip() if review_tag else "N/A"
 
     # Extract laptop product information
-    info_tag = product.select_one("div._6NESgJ ul.G4BRas")
+    info_tag = laptop.select_one("div._6NESgJ ul.G4BRas")
     product_info = [li.text for li in info_tag.find_all("li")] if info_tag else "N/A"
 
     # Extract laptop original price
-    original_price_tag = product.select_one("div.BfVC2z div.ZYYwLA")
+    original_price_tag = laptop.select_one("div.BfVC2z div.ZYYwLA")
     original_price = original_price_tag.text if original_price_tag else "N/A"
 
     # Extract laptop discount
-    discount_tag = product.find("div", class_="UkUFwK")
+    discount_tag = laptop.select_one("div.UkUFwK")
     discount = discount_tag.text if discount_tag else "N/A"
 
     # Extract laptop discounted price
-    discounted_price_tag = product.select_one("div.BfVC2z > div.cN1yYO div._4b5DiR")
+    discounted_price_tag = laptop.select_one("div.BfVC2z > div.cN1yYO div._4b5DiR")
     discounted_price = discounted_price_tag.text if discounted_price_tag else "N/A"
 
     # Check if a bank offer is available
-    bank_offer_tag = product.select_one("div.M4DNwV div.n5vj9c div.O5Fpg8")
+    bank_offer_tag = laptop.select_one("div.M4DNwV div.n5vj9c div.O5Fpg8")
     is_bank_offer = "Yes" if bank_offer_tag else "No"
 
     # Print the extracted data
